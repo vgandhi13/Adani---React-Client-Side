bb5b6f73e21d0aecbfc610a2afec008f2fd9b9a6
Varun <vgandhi@umass.edu>
2023-07-10 15:02:34 +0530

    fixed reload error

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,7 +4,11 @@ const Sidebar = ({userObj}) => {
          <div class="col-md-3 col-lg-2 sidebar-offcanvas pl-0" id="sidebar" role="navigation" style={{backgroundColor:"#e9ecef"}}>
          {/* <img style={{marginTop:'20px' ,width: '8rem'}} src='/Adani-Logo.jpeg' alt='Logo'></img> */}
             <ul class="nav flex-column sticky-top pl-0 pt-5 p-3 mt-3 ">
-                <li class="nav-item mb-2 mt-3"><h5>{userObj.first_name} {userObj.last_name}</h5></li>
+                {userObj && (
+                    <li className="nav-item mb-2 mt-3">
+                        <h5>{userObj.first_name} {userObj.last_name}</h5>
+                    </li>
+                )}
                 <li class="nav-item mb-2 "><a class="nav-link text-secondary" href="/"><i class="fas fa-user font-weight-bold"></i> <span className="ml-3">Overview</span></a></li>
                 <li class="nav-item mb-2">
                     <a class="nav-link text-secondary" href="#submenu1" data-toggle="collapse" data-target="#submenu1"><i class="far fa-file-word font-weight-bold"></i> <span className="ml-3"> Reports▾</span></a>
