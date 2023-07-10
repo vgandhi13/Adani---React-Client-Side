39394e71398f99d40e78973accb57e1206f67e83
Varun <vgandhi@umass.edu>
2023-07-10 15:20:09 +0530

    error

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,20 +1,22 @@
+import React, { useEffect, useState } from 'react';
 import Navbar from "../components/Navbar";
 import SelectionBox from "../components/SelectionBox";
-import React, { useEffect, useState } from 'react';
 import './LandingPage.css';
 import AuthVerification from "../Services/AuthVerification";
 
 const Selection = () => {
-  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    return (
-        <AuthVerification>
-            <div className='app'>
-                <Navbar />
-                <SelectionBox />
-            </div>
-        </AuthVerification>
-    )
-}
+  return (
+    <AuthVerification onAuthChange={setIsAuthenticated}>
+      {isAuthenticated && (
+        <div className='app'>
+          <Navbar />
+          <SelectionBox />
+        </div>
+      )}
+    </AuthVerification>
+  );
+};
 
-export default  Selection;
+export default Selection;
--- src/Services/AuthVerification.js
+++ src/Services/AuthVerification.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { authenticate } from './Requests';
 import { useNavigate } from 'react-router-dom';
 
-const AuthVerification = ({ children }) => {
+const AuthVerification = ({ children, onAuthChange }) => {
   const navigator = useNavigate();
   useEffect(() => {
     const verifyToken = async () => {
@@ -11,17 +11,20 @@ const AuthVerification = ({ children }) => {
       if (storedToken) {
         try {
           // Send API request to verify token
-          let response = await authenticate(storedToken)
-          console.log(response)
+          let response = await authenticate(storedToken);
+          console.log(response);
+          onAuthChange(true); // Notify the parent component about successful authentication
         } catch (error) {
           // Handle API error
           console.error(error);
+          onAuthChange(false); // Notify the parent component about authentication failure
         }
       } else {
         // Token doesn't exist in storage, handle accordingly
         // e.g., setAuthenticated(false);
-        navigator('/')
-        console.log('Token doesnt exist in storage, handle accordingly')
+        navigator('/');
+        console.log('Token doesn\'t exist in storage, handle accordingly');
+        onAuthChange(false); // Notify the parent component about authentication failure
       }
     };
 
